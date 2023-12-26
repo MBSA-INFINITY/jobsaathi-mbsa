f0692bb6cad5b77711c60c1fcad3dcfae0b34efb
MBSA-INFINITY <mbsaiaditya@gmail.com>
2023-12-26 21:15:59 +0530

    removed unnecessary helpers

--- helpers.py
+++ helpers.py
@@ -13,29 +13,6 @@ template = """You are a chatbot who helps people to build their resume/portfolio
 prompt = PromptTemplate(template=template, input_variables=["html", "statement"])
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-def random_string_generator(N):
-    res = ''.join(random.choices(string.ascii_lowercase +
-                             string.digits, k=N))
-    return str(res)
-
-def random_string_generator_only_alpha(N):
-    res = ''.join(random.choices(string.ascii_lowercase +
-                             string.ascii_uppercase, k=N))
-    return str(res)
-
-
-def is_valid_url(url):
-    # Define a regular expression pattern to match URLs
-    url_pattern = re.compile(
-        r'^(https?|ftp)://'  # Match the scheme (http, https, or ftp)
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # Match domain
-        r'localhost|'  # Match "localhost"
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # Match IP address
-        r'(?::\d+)?'  # Match optional port number
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-    return bool(url_pattern.match(url))
-
 def query_update_billbot(user_id, statement):
     resume_html = get_resume_html_db(user_id)
     html_code = llm_chain.run({"html": str(resume_html), "statement": statement}) 
